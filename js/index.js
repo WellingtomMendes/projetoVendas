3e14bc59fea6159c3cd3baad4b45aa49f2cf0cc1
Wellingtom Mendes <wellingtommendeslandim00@gmail.com>
2024-05-26 10:50:08 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+"use stric"
+
+const esquerdo = document.querySelector(".esquerdo");
+const modelo1 = document.querySelector("#modelo1");
+
+esquerdo.addEventListener("click", function(){
+
+    if(!document.querySelector(".detalhesCarro1")){
+
+        let item = document.createElement("div");
+        item.classList.add("detalhesCarro1");
+    
+        let containerTitulo = document.createElement("nav");
+        containerTitulo.classList.add("containerNomeCarro");
+    
+        let titulo = document.createElement("h4");
+        titulo.classList.add("tituloCarro");
+        titulo.textContent = "Porsche Cayene"
+
+        let botao = document.createElement("bottom");
+        botao.classList.add("deletarDetalhes");
+        botao.textContent = "Fechar"
+
+        item.appendChild(containerTitulo);
+        containerTitulo.appendChild(titulo);
+        containerTitulo.appendChild(botao)
+    
+        modelo1.appendChild(item);    
+        
+        let del = document.querySelector(".deletarDetalhes");
+
+        del.addEventListener("click", ()=>{ modelo1.removeChild(item)})
+
+    }else{
+        alert("Já está com os detalhes ")
+    }
+
+
+})
